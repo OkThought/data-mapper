35b6390a7f5a4ff3cf0a9d74ff552e359b061056
OkThought <bogush.vano@gmail.com>
2019-08-10 12:51:17 +0700

    Merge TransformableProperty and Property

--- src/properties/property.py
+++ src/properties/property.py
@@ -1,33 +1,54 @@
-from typing import Iterable
+from typing import Iterable, Callable
 
 from src.errors import PropertyNotFound
-from src.properties.transformable import TransformableProperty
+from src.properties.abstract import AbstractProperty
 from src.utils import NOT_SET
 
 
-class Property(TransformableProperty):
+class Property(AbstractProperty):
     required = True
     default = NOT_SET
     sources = None
+    transforms = []
 
     def __init__(
             self,
             sources: Iterable = NOT_SET,
             default=NOT_SET,
             required: bool = NOT_SET,
+            transforms: Iterable[Callable[[str], str]] = NOT_SET,
             **kwargs,
     ):
+        assert default is NOT_SET or not required
+
         super().__init__(**kwargs)
 
         if sources is not NOT_SET:
             self.sources = sources
 
-        assert default is NOT_SET or not required
-
         if default is not NOT_SET:
             self.default = default
+
         if self.required is not NOT_SET:
             self.required = required
+
+        if transforms is not NOT_SET:
+            self.transforms = transforms
+
+    def get(self, data: dict, result=None):
+        value = self.get_raw(data, result)
+        self.validate_raw(value)
+        value = self.transform(value)
+        self.validate(value)
+        return value
+
+    def transform(self, value):
+        for transform in self.get_transforms():
+            value = transform(value)
+        return value
+
+    def get_transforms(self):
+        return self.transforms
 
     def get_raw(self, data: dict, result=None):
         assert self.sources, \
