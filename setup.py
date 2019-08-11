16ff0b7d542a4beea3277daf48a498defd760be9
OkThought <bogush.vano@gmail.com>
2019-08-11 13:54:05 +0700

    Add package setup

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="data-mapper",
+    version="0.0.0",
+    author="OkThought",
+    author_email="bogush.vano@gmai.com",
+    description="A declarative data mapper",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/OkThought/data-mapper",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
