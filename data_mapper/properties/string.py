b434704b56769483df80e1a40696cdbee8411f99
OkThought <bogush.vano@gmail.com>
2019-08-14 12:47:16 +0700

    Fix StringProperty

--- data_mapper/properties/string.py
+++ data_mapper/properties/string.py
@@ -2,5 +2,8 @@ from data_mapper.properties.property import Property
 
 
 class StringProperty(Property):
-    def transform(self, value: str):
-        return super().transform(str(value))
+    def get_raw(self, data, result=None):
+        value = super().get_raw(data, result)
+        if value is not None:
+            value = str(value)
+        return value
