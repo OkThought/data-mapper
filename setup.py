2b0c7b167d0829679537f89e3fcf361d79f520f8
OkThought <bogush.vano@gmail.com>
2019-08-16 15:19:50 +0700

    Bump version: 1.1.0 → 2.0.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="data-mapper",
-    version="1.1.0",
+    version="2.0.0",
     author="OkThought",
     author_email="bogush.vano@gmail.com",
     description="A declarative data mapper",
